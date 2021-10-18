c1d049bf7703159ff5b8c648387d54c6b0294b0f
burn4live <ericb@linux.com>
2021-10-18 16:48:57 -0600

    refactored forms and added upload / edit profile

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,5 +1,6 @@
 import { Field, Form, Formik, FormikProps } from "formik";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useS3Upload } from "next-s3-upload";
 
 const Input = ({ field, form, ...props }) => {
   return (
@@ -21,35 +22,51 @@ const Section = ({ name, children }) => (
 );
 
 function ProfileForm({ user }) {
-  const [nextform, setNext] = useState(1);
+  const [isUploadComplete, setIsUploadComplete] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { uploadToS3 } = useS3Upload();
+
+  const handleFilesChange = async ({ target }) => {
+    setLoading(true);
+    const urls = [];
+    const files = Array.from(target.files);
+
+    for (let index = 0; index < files.length; index++) {
+      const file = files[index];
+      const { url } = await uploadToS3(file);
+      urls.push(url);
+    }
+    setIsUploadComplete(true);
+    setLoading(false);
+    updateProfile({ documents: urls });
+  };
+
+  async function updateProfile(info) {
+    const response = await fetch("/api/user/update-profile", {
+      method: "PATCH",
+      body: JSON.stringify(info),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+  }
+  console.log(user);
   return (
     <section className="h-screen bg-gray-100 bg-opacity-50">
       <Formik
         initialValues={{
           name: user?.name || "",
-          phone: "",
-          address: "",
-          city: "",
-          province: "",
-          postal: "",
-          cpr: "",
-          driving: "",
-          degree: "",
-          teaching: "",
-          ncs: "",
-          ics: "",
-          rn: "",
-          fa: "",
-          cna: "",
-          ecd: "",
-          bsp: "",
+          phone: user?.phone || "",
+          address: user?.address || "",
+          city: user?.city || "",
+          province: user?.province || "",
+          country: user?.country | "",
+          postal: user?.postal || "",
         }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-
-            actions.setSubmitting(false);
-          }, 1000);
+        onSubmit={async (values) => {
+          await updateProfile(values);
         }}
       >
         <Form className="container max-w-2xl mx-auto shadow-md md:w-3/4">
@@ -62,132 +79,79 @@ function ProfileForm({ user }) {
             </div>
           </div>
           <div className="space-y-6 bg-white">
-            {nextform === 1 && (
-              <>
-                <Section name="Your Email">
-                  <Field
-                    disabled
-                    type="email"
-                    name="email"
-                    defaultValue={user.email}
-                    component={Input}
-                  />
-                </Section>
-                <hr />
-                <Section name="Personal Information">
-                  <Field
-                    type="text"
-                    name="name"
-                    placeholder="Your Full Name"
-                    component={Input}
-                  />
-                  <Field
-                    type="tel"
-                    name="phone"
-                    placeholder="Phone Number"
-                    component={Input}
-                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                  />
-                  <Field
-                    type="text"
-                    name="address"
-                    placeholder="Address"
-                    component={Input}
-                  />
-                  <Field
-                    type="text"
-                    name="city"
-                    placeholder="city"
-                    component={Input}
-                  />
-                  <Field
-                    type="text"
-                    name="province"
-                    placeholder="Province"
-                    component={Input}
-                  />
-                  <Field
-                    type="text"
-                    name="country"
-                    placeholder="Country"
-                    component={Input}
-                  />
-                  <Field
-                    type="text"
-                    name="postal"
-                    placeholder="Postal code"
-                    component={Input}
-                  />
-                </Section>
-                <hr />
-
-                <div className="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
-                  <button
-                    onClick={() => setNext(2)}
-                    className="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                  >
-                    Next
-                  </button>
-                </div>
-                <hr />
-              </>
-            )}
-            {nextform === 2 && (
-              <>
-                <Section name="CPR Certification">
-                  <Field type="file" name="cpr" component={Input} />
-                </Section>
-                <Section name="Driving License">
-                  <Field type="file" name="driving" component={Input} />
-                </Section>
-                <Section name="Degree Certificate">
-                  <Field type="file" name="degree" component={Input} />
-                </Section>
-                <Section name="Teaching Certification">
-                  <Field type="file" name="teaching" component={Input} />
-                </Section>
-                <Section name="Newborn Care Specialist (NCS)">
-                  <Field type="file" name="ncs" component={Input} />
-                </Section>
-                <Section name="Infant Care Specialist (ICS)">
-                  <Field type="file" name="ics" component={Input} />
-                </Section>
-                <Section name="Registered Nurse (RN)">
-                  <Field type="file" name="rn" component={Input} />
-                </Section>
-                <Section name="First Aid Certification">
-                  <Field type="file" name="fa" component={Input} />
-                </Section>
-                <Section name="Certified Nursing Assistant (CNA)">
-                  <Field type="file" name="cna" component={Input} />
-                </Section>
-                <Section name="Early Childhood Development (ECD)">
-                  <Field type="file" name="ecd" component={Input} />
-                </Section>
-                <Section name="Babysitpro Certificate (BSP)">
-                  <Field type="file" name="bsp" component={Input} />
-                </Section>
-                <hr />
-                <div className="flex flex-wrap">
-                  <div className="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
-                    <button
-                      onClick={() => setNext(1)}
-                      className="py-2 px-4  bg-yellow-600 hover:bg-yellow-700 focus:ring-yellow-500 focus:ring-offset-yellow-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                    >
-                      back
-                    </button>
-                  </div>
-                  <div className="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
-                    <button
-                      type="submit"
-                      className="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                    >
-                      Save
-                    </button>
-                  </div>
-                </div>
-              </>
-            )}
+            <Section name="Your Email"></Section>
+            <hr />
+            <Section name="Personal Information">
+              <Field
+                type="text"
+                name="name"
+                placeholder="Your Full Name"
+                component={Input}
+              />
+              <Field
+                type="text"
+                name="phone"
+                placeholder="Phone Number"
+                component={Input}
+              />
+              <Field
+                type="text"
+                name="address"
+                placeholder="Address"
+                component={Input}
+              />
+              <Field
+                type="text"
+                name="city"
+                placeholder="city"
+                component={Input}
+              />
+              <Field
+                type="text"
+                name="province"
+                placeholder="Province"
+                component={Input}
+              />
+              <Field
+                type="text"
+                name="country"
+                placeholder="Country"
+                component={Input}
+              />
+              <Field
+                type="text"
+                name="postal"
+                placeholder="Postal code"
+                component={Input}
+              />
+            </Section>
+            <hr />
+            <div>
+              <p>Include all your documents and certifications</p>
+              {!loading ? (
+                <Input
+                  type="file"
+                  name="file"
+                  multiple
+                  onChange={handleFilesChange}
+                />
+              ) : (
+                <p>Loading ...</p>
+              )}
+              <br />
+              {isUploadComplete && "Done Uploading files to S3"}
+              {/* TODO LIST ALL DOCUMENTS UPLOADED */}
+            </div>
+            <hr />
+            <div className="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
+              <button
+                type="submit"
+                className="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                Next
+              </button>
+            </div>
+            <hr />
           </div>
         </Form>
       </Formik>
