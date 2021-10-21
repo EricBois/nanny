10f79c93f834a4557f98ef633eae51188a9fdf43
Antonio Thomas <antoniothomasjr@gmail.com>
2021-10-21 13:04:21 -0500

    added create new customer and link to mongo, and create-portal-sesion enpoint

--- pages/api/checkout-session/index.js
+++ pages/api/checkout-session/index.js
@@ -1,16 +1,45 @@
+import { connectToDatabase } from "lib/db";
+import { getSession } from "next-auth/client";
 import stripe from "utils/stripe/stripe";
 
 const sessions = async (req, res) => {
+  if (req.method !== "POST") {
+    return;
+  }
   const DOMAIN = req.headers.origin;
+  const authSession = await getSession({ req });
+
+  if (!authSession) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
+
+  const client = await connectToDatabase();
+  const usersCollection = client.db().collection("users");
+
+  const currentUser = await usersCollection.findOne(
+    {
+      email: authSession.user.email,
+    },
+    { projection: { password: 0 } }
+  );
+
+  //todo: create stripe customer
+
+  const newCustomer = await stripe.customers.create({
+    email: currentUser.email,
+    description: "New Customer",
+  });
 
   const prices = await stripe.prices.list({
     lookup_keys: [req.body.lookup_key],
     expand: ["data.product"],
   });
 
-  const session = await stripe.checkout.sessions.create({
+  const stripeSession = await stripe.checkout.sessions.create({
     billing_address_collection: "auto",
     payment_method_types: ["card"],
+    customer: newCustomer.id,
     line_items: [
       {
         price: prices.data[0].id,
@@ -25,7 +54,18 @@ const sessions = async (req, res) => {
     cancel_url: `${DOMAIN}/?canceled=true`,
   });
 
-  res.status(200).json({ sessionId: session.id });
+  // console.log(currentUser);
+  // console.log(newCustomer);
+  // console.log(req);
+  // console.log(authSession);
+  // console.log(stripeSession);
+
+  const result = usersCollection.updateOne(
+    { email: authSession.user.email },
+    { $set: { customerId: stripeSession.customer } }
+  );
+
+  res.status(200).json({ sessionId: stripeSession.id });
 };
 
 export default sessions;
---
+++ pages/api/create-portal-session/index.js
@@ -0,0 +1,35 @@
+import { connectToDatabase } from "lib/db";
+import { getSession } from "next-auth/client";
+import { stripe } from "utils/stripe";
+
+const createPortalSession = async (req, res) => {
+  if (req.method !== "POST") {
+    return;
+  }
+  const authSession = await getSession({ req });
+  if (!authSession) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
+
+  const client = await connectToDatabase();
+  const usersCollection = client.db().collection("users");
+
+  const user = await usersCollection.findOne({
+    email: authSession.user.email,
+  });
+
+  if (!user.customerId)
+    throw new Error("oops! looks like you still need to choose a package");
+
+  const returnUrl = req.headers.origin;
+
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: user.customerId,
+    return_url: returnUrl,
+  });
+
+  return res.status(200).json({ portalUrl: portalSession.url });
+};
+
+export default createPortalSession;
