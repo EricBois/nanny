347d268b2d27c32ffff24d608b35d55f702bf3f0
burn4live <ericb@linux.com>
2021-10-23 07:57:00 -0600

    added places autocomplete

--- components/validation/validationProfileSchema.js
+++ components/validation/validationProfileSchema.js
@@ -3,15 +3,10 @@ import * as yup from "yup";
 const validationProfileSchema = yup.object({
   name: yup.string("Enter your name").required("Email is required"),
   phone: yup.string("Enter your phone").required("Phone is required"),
-  address: yup
-    .string("Enter your Full Address")
-    .required("Address is required"),
-  city: yup.string("Enter your city").required("City is required"),
-  province: yup.string("Enter your province").required("Province is required"),
-  country: yup.string("Enter your country").required("Country is required"),
-  postal: yup
-    .string("Enter your Postal Code")
-    .required("Postal Code is required"),
+  location: yup.object().shape({
+    value: yup.string().required("Address is required"),
+    address: yup.string().required("Invalid address"),
+  }),
 });
 
 export default validationProfileSchema;
