4958bc28f2a030d8c457f781ac75ea272715a391
Antonio Thomas <antoniothomasjr@gmail.com>
2021-10-28 16:43:55 -0500

    pusher base

---
+++ pages/chat.js
@@ -0,0 +1,23 @@
+import ChatContent from "components/chat-content";
+import { getSession } from "next-auth/client";
+
+export default function Chat({ session }) {
+  console.log(session);
+  return <ChatContent />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
